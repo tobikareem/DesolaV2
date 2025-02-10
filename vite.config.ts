b63f003305eb0e0e9354b3ccfb69fbef773874bc
tobikareem <toboibo@yahoo.com>
2025-02-09 23:13:54 -0800

    Feature: Update build scripts and enhance Vite configuration with Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true, // TODO: set to false for production
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
 })
