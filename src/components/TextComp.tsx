04fdfacc836e770aff602270a8df26224e1c5986
tobikareem <toboibo@yahoo.com>
2025-02-11 15:12:01 -0800

    Feat: Add local 'horizon' dependency and update TextComp props type

--- src/components/TextComp.tsx
+++ src/components/TextComp.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 interface TextProps {
   children: ReactNode;
   as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
-  className?: string | undefined;
+  className?: string;
   size?: "sm" | "base" | "lg" | "xl" | "2xl" | "3xl" | "4xl";
   weight?: "light" | "normal" | "medium" | "semibold" | "bold" | "extrabold";
   color?: string;
