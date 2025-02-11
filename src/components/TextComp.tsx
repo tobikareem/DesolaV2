79b8da35be9ec9d00ae1ac7b1e825069b26c69fb
bagwells <timmyayomidebagwells@outlook.com>
2025-02-12 00:29:45 +0100

    feat:Home Page Hero section

--- src/components/TextComp.tsx
+++ src/components/TextComp.tsx
@@ -4,7 +4,7 @@ interface TextProps {
   children: ReactNode;
   as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p";
   className?: string | undefined;
-  size?: "sm" | "base" | "lg" | "xl" | "2xl" | "3xl" | "4xl";
+  size?: "sm" | "base" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "6xl";
   weight?: "light" | "normal" | "medium" | "semibold" | "bold" | "extrabold";
   color?: string;
 }
