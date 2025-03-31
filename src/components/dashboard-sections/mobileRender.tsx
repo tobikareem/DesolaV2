26a05448fee5b999aef23803c1f5106cb0654896
tobikareem <toboibo@yahoo.com>
2025-03-31 10:24:30 -0700

    Refactor MobileRender and Navbar components to use NavigationContext and ChatContext for improved state management

--- src/components/dashboard-sections/mobileRender.tsx
+++ src/components/dashboard-sections/mobileRender.tsx
@@ -1,22 +1,31 @@
 
+import React, { useContext } from "react";
+import { ChatContext } from "../../contexts/ChatContext";
 import { PathContent } from "../../pages/dashboard/sections/PathContent";
 import { SupportContent } from "../../pages/dashboard/sections/SupportContent";
 import { TrashContent } from "../../pages/dashboard/sections/TrashContent";
 import { UserContent } from "../../pages/dashboard/sections/UserContent";
-import React from "react";
-import { GlobalContext } from "../../hooks/globalContext";
+import { NavigationContext } from "../../contexts/NavigationContext";
 
 
 
-const MobileRender:React.FC=()=> {
+const MobileRender: React.FC = () => {
 
-  const {MobileTab} = React.useContext(GlobalContext);
-  
+  const { mobileTab } = useContext(NavigationContext);
+  const { travelInfo } = useContext(ChatContext);
+
   const RenderPage = () => {
-    switch (MobileTab) {
+    switch (mobileTab) {
       case 'road':
         return (
-          <PathContent departure={''} destination={''} departureDate={''} returnDate={''} travelRoute={''} flightClass={''} />
+          <PathContent
+            departure={travelInfo.departure}
+            destination={travelInfo.destination}
+            departureDate={travelInfo.departureDate}
+            returnDate={travelInfo.returnDate}
+            travelRoute={travelInfo.travelRoute}
+            flightClass={travelInfo.flightClass}
+          />
         );
 
       case 'trash':
@@ -33,11 +42,14 @@ const MobileRender:React.FC=()=> {
         return (
           <SupportContent />
         );
+
+      default:
+        return null;
     }
   };
 
-  return(
-    <div className={`absolute ${MobileTab !== '' ? 'block':'hidden'} bg-white lg:hidden w-screen h-screen py-8`}>
+  return (
+    <div className={`absolute ${mobileTab !== '' ? 'block' : 'hidden'} bg-white lg:hidden w-screen h-screen py-8`}>
       <div className="w-full h-full pt-10 px-6">
         {RenderPage()}
       </div>
