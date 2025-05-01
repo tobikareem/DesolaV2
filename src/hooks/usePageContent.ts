6e3959a8dae13b6311e92b9c25bd6a62b740935e
bagwells <timmyayomidebagwells@outlook.com>
2025-05-01 13:12:26 +0100

    pricing page and Subscription component

--- src/hooks/usePageContent.ts
+++ src/hooks/usePageContent.ts
@@ -7,10 +7,30 @@ const usePageContent = (path:string, partitionKey: string, rowKey: string) => {
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
+
   useEffect(() => {
+    const cacheExpiryTime = 30 * 24 * 60 * 60 * 1000;
+    const cacheKey = `pageContent_${path}_${partitionKey}_${rowKey}`;
+    
+
     const fetchContent = async () => {
+      const cachedDataString = localStorage.getItem(cacheKey);
       try {
+        if(cachedDataString){
+          const now = new Date().getTime();
+          const cachedData = JSON.parse(cachedDataString);
+          if(now - cachedData.timestamp < cacheExpiryTime){
+            setContent(cachedData.data);
+            setLoading(false);
+            return;
+          }
+        }
         const data: PageResponse = await getPageSection(path, partitionKey, rowKey);
+        const dataToCache = {
+          data, timestamp: new Date().getTime(),
+        }
+        localStorage.setItem(cacheKey, JSON.stringify(dataToCache));
+
         setContent(data);
       } catch {
         setError(`Failed to fetch content`);
