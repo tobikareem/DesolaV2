6bc4616315f2685c9663cf355c34fdbd947974b1
Adeola-creator <111973534+deeygitalstudio@users.noreply.github.com>
2025-05-02 08:32:56 +0100

    ui fix trip comp

--- src/pages/dashboard/sections/PathContent.tsx
+++ src/pages/dashboard/sections/PathContent.tsx
@@ -1,6 +1,6 @@
 import { SlidersHorizontal, PlaneTakeoff, CalendarCheck, PlaneLanding, Plane, Route } from "lucide-react";
 import { Text } from "../../../components/ui/TextComp"
-
+import { Btn } from "../../../components/ui/Button";
 
 interface PathContentProps {
   departure: string;
@@ -26,7 +26,7 @@ export const PathContent: React.FC<PathContentProps> = ({
     year: 'numeric'
   });
 
-  return(
+  return (
     <div className=" flex-1 overflow-y-auto">
       <Text
         as="h1"
@@ -37,122 +37,115 @@ export const PathContent: React.FC<PathContentProps> = ({
         My Trips
       </Text>
       <div className="flex items-center h-['100px'] mb-2 pb-3 gap-3 bg-white w-full">
-        <Text
-          as="p"
-          size="sm"
-          className="bg-primary-100 py-1.5 px-2.5 rounded-lg"
-        >
+        <Btn className="bg-primary-100 cursor-pointer" size="sm">
           Today
-        </Text>
-        <Text as="p" size="sm" className="cursor-pointer">
+        </Btn>
+        <Btn className="cursor-pointer" size="sm">
           Yesterday
-        </Text>
-        <Text as="p" size="sm" className="cursor-pointer">
+        </Btn>
+        <Btn size="sm" className="cursor-pointer">
           1 Week ago
-        </Text>
-        <div className="flex items-center cursor-pointer gap-2">
-          <Text as="p" size="sm" className="">
-            Filter
-          </Text>
+        </Btn>
+        <Btn className="flex items-center cursor-pointer gap-2" size="sm">
+          <Text className="">Filter</Text>
           <SlidersHorizontal className="mt-0.5" size={14} />
-        </div>
+        </Btn>
       </div>
-    {
-      <div className="flex flex-col gap-4 py-4 border-t border-t-neutral-300">
-        <Text as="p" size="xs" className="font-work">
-          {today}
-        </Text>
-        <div className="space-y-2">
-          <div className="flex items-center gap-2">
-            <PlaneTakeoff
-              className="text-secondary-700 hover:scale-110 transition duration-300"
-              size={20}
-            />
-            <label className="font-grotesk block !text-xl font-bold text-neutral">
-              Departure
-            </label>
-          </div>
-          <Text as="p" className="font-work text-xs text-neutral-500">
-            {departure}
+      {
+        <div className="flex flex-col gap-4 py-4 border-t border-t-neutral-300">
+          <Text as="p" size="xs" className="font-work">
+            {today}
           </Text>
-        </div>
-        <div className="space-y-2">
-        <div className="flex items-center gap-2">
-          <Route
-            size={20}
-            className="text-primary-700 hover:scale-110 transition duration-300"
-          />
-          <label className="font-grotesk block !text-xl font-bold text-neutral">
-            Destination
-          </label>
-        </div>
-        <Text as="p" className="font-work text-xs text-neutral-500">
-          {destination}
-        </Text>
-      </div>
+          <div className="space-y-2">
+            <div className="flex items-center gap-2">
+              <PlaneTakeoff
+                className="text-secondary-700 hover:scale-110 transition duration-300"
+                size={20}
+              />
+              <label className="font-grotesk block !text-xl font-bold text-neutral">
+                Departure
+              </label>
+            </div>
+            <Text as="p" className="font-work text-xs text-neutral-500">
+              {departure}
+            </Text>
+          </div>
+          <div className="space-y-2">
+            <div className="flex items-center gap-2">
+              <Route
+                size={20}
+                className="text-primary-700 hover:scale-110 transition duration-300"
+              />
+              <label className="font-grotesk block !text-xl font-bold text-neutral">
+                Destination
+              </label>
+            </div>
+            <Text as="p" className="font-work text-xs text-neutral-500">
+              {destination}
+            </Text>
+          </div>
 
-      <div className="space-y-2">
-        <div className="flex items-center gap-2">
-          <CalendarCheck
-            className="text-success hover:scale-110 transition duration-300"
-            size={20}
-          />
-          <label className="font-grotesk block !text-xl font-bold text-neutral">
-            Departure Date
-          </label>
-        </div>
-        <Text as="p" className="font-work  text-xs text-neutral-500">
-          {departureDate}
-        </Text>
-      </div>
+          <div className="space-y-2">
+            <div className="flex items-center gap-2">
+              <CalendarCheck
+                className="text-success hover:scale-110 transition duration-300"
+                size={20}
+              />
+              <label className="font-grotesk block !text-xl font-bold text-neutral">
+                Departure Date
+              </label>
+            </div>
+            <Text as="p" className="font-work  text-xs text-neutral-500">
+              {departureDate}
+            </Text>
+          </div>
 
-      <div className="space-y-2">
-        <div className="flex items-center gap-2">
-          <CalendarCheck
-            className="text-primary-300 hover:scale-110 transition duration-300"
-            size={20}
-          />
-          <label className="font-grotesk block !text-xl font-bold text-neutral">
-            Returning Date
-          </label>
-        </div>
-        <Text as="p" className="font-work text-xs text-neutral-500">
-          {returnDate ?? "Not Available"}
-        </Text>
-      </div>
+          <div className="space-y-2">
+            <div className="flex items-center gap-2">
+              <CalendarCheck
+                className="text-primary-300 hover:scale-110 transition duration-300"
+                size={20}
+              />
+              <label className="font-grotesk block !text-xl font-bold text-neutral">
+                Returning Date
+              </label>
+            </div>
+            <Text as="p" className="font-work text-xs text-neutral-500">
+              {returnDate ?? 'Not Available'}
+            </Text>
+          </div>
 
-      <div className="space-y-2">
-        <div className="flex items-center gap-2">
-          <PlaneLanding
-            className="text-primary-600 hover:scale-110 transition duration-300"
-            size={20}
-          />
-          <label className="font-grotesk block !text-xl font-bold text-neutral">
-            Travel Route
-          </label>
-        </div>
-        <Text as="p" className="font-work mt-1 text-xs text-neutral-500">
-          {travelRoute}
-        </Text>
-      </div>
+          <div className="space-y-2">
+            <div className="flex items-center gap-2">
+              <PlaneLanding
+                className="text-primary-600 hover:scale-110 transition duration-300"
+                size={20}
+              />
+              <label className="font-grotesk block !text-xl font-bold text-neutral">
+                Travel Route
+              </label>
+            </div>
+            <Text as="p" className="font-work mt-1 text-xs text-neutral-500">
+              {travelRoute}
+            </Text>
+          </div>
 
-      <div className="space-y-2">
-        <div className="flex items-center gap-2">
-          <Plane
-            className="text-secondary-700 hover:scale-110 transition duration-300"
-            size={20}
-          />
-          <label className="font-grotesk block !text-xl font-bold text-neutral">
-            Flight
-          </label>
+          <div className="space-y-2">
+            <div className="flex items-center gap-2">
+              <Plane
+                className="text-secondary-700 hover:scale-110 transition duration-300"
+                size={20}
+              />
+              <label className="font-grotesk block !text-xl font-bold text-neutral">
+                Flight
+              </label>
+            </div>
+            <Text as="p" className="font-work mt-1 text-xs text-neutral-500">
+              {flightClass}
+            </Text>
+          </div>
         </div>
-        <Text as="p" className="font-work mt-1 text-xs text-neutral-500">
-          {flightClass}
-        </Text>
-      </div>
-
+      }
     </div>
-    }
-    </div>
-  )
+  );
 }
