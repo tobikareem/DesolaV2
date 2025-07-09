43e133bcdc2f42e08b5eb1c666ba4f160546a913
bagwells <timmyayomidebagwells@outlook.com>
2025-07-09 02:13:03 +0100

    ...Subscrition

--- src/components/modals/SubscriptionFlow.tsx
+++ src/components/modals/SubscriptionFlow.tsx
@@ -1,19 +1,21 @@
-
 import { useModals } from "../../hooks/useModals";
 import { useSubscription } from "../../hooks/useSubscription";
 import SubscriptionModal from "./SubscriptionModal";
 import { Modal } from "./Modal"
 import { CustomStorage } from "../../utils/customStorage";
-import { useEffect } from "react";
-import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
 import { useAuthInfo } from "../../hooks/useAuthInfo";
+import { useCustomerApi } from "../../hooks/useCustomerApi";
+import { toast } from "react-toastify";
 
 
 export const SubscriptionFlowModal =({Action}:{Action:()=>void})=> {
-  const { isAuthenticated, isProfileComplete } = useAuthInfo();
+  const {customerProfile} = useAuthInfo();
   const {stage} = useSubscription();
   const {toggleSubscriptionModal, showSubscriptionModal} = useModals();
   const storage = new CustomStorage();
+  const {getCustomerByEmail} = useCustomerApi();
+  const [isSubscribed, setIsSubscribed] = useState<boolean | null>(null);
 
 
 
@@ -30,18 +32,29 @@ export const SubscriptionFlowModal =({Action}:{Action:()=>void})=> {
 
   useEffect(() => {
     const firstTimeLoad = storage.getItem('Subscription') === 'true';
-    if(!firstTimeLoad && !isProfileComplete && isAuthenticated) {
-      toggleSubscriptionModal();
-      storage.setItem('Subscription', 'true');
-      toast.info('Please subscribe to our service to access all features.');
-      return;
+    const email = customerProfile?.email;
+    async function checkSubscription() {
+      if (email && !firstTimeLoad) {
+        try{
+          const customer = await getCustomerByEmail(email);
+          storage.setItem('Subscription', 'true');
+          setIsSubscribed(!!customer?.hasActiveSubscription);
+          if(!isSubscribed){
+            toggleSubscriptionModal()
+          }
+        } catch (error) {
+          toast.error(`Error fetching customer subscription: ${error instanceof Error ? error.message : String(error)}`);
+        }
+      }
+
     }
 
+    checkSubscription()
   },[])
 
   return(
     <>
-      { showSubscriptionModal && !isProfileComplete && isAuthenticated && (
+      { showSubscriptionModal && !isSubscribed && (
         <Modal className={`backdrop-blur-sm`} display={showSubscriptionModal} close={toggleSubscriptionModal}>
           {subscriptionFlowRender()}
         </Modal>)
