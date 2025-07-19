b2ff97d63deb98f2c0866c3d4ceab2db241ac70f
Adeola-creator <111973534+deeygitalstudio@users.noreply.github.com>
2025-07-19 12:27:01 +0100

    AdminDashboardChart

--- src/components/AdminComponents/DashboardView.tsx
+++ src/components/AdminComponents/DashboardView.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { DashboardStats } from '../ui/AdminUIComp/DashBoardStats';
+import { DashboardCharts } from '../ui/AdminUIComp/DashBoardCharts';
 
 const DashboardView = () => {
   return <div> 
   <DashboardStats />
+  <DashboardCharts />
   </div>;
 };
 
